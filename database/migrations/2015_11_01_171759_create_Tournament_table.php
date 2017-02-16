8e26fbc4e8ecf1ceb0362183fc7a5e7e6f2eb9b7
julien <julien@t4b.mx>
2017-02-16 11:57:08 -0600

    Remove slug from tournament

--- database/migrations/2015_11_01_171759_create_Tournament_table.php
+++ database/migrations/2015_11_01_171759_create_Tournament_table.php
@@ -18,8 +18,6 @@ class CreateTournamentTable extends Migration {
 					->onDelete('cascade');
 
 			$table->string('name');
-			$table->string('slug')->default('')->unique();
-
 			$table->date('dateIni');
 			$table->date('dateFin');
             $table->date('registerDateLimit');
