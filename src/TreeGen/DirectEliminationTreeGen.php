1006b0ad3afdc11f5ecde8435126189e41ef3d06
julien <julien@t4b.mx>
2017-02-27 13:14:55 -0600

    Refactoring and improvements

--- src/TreeGen/DirectEliminationTreeGen.php
+++ src/TreeGen/DirectEliminationTreeGen.php
@@ -27,113 +27,35 @@ class DirectEliminationTreeGen implements TreeGenerable
 
     public function run()
     {
-
+        $teams = [];
         //If no names have been entered, then use numbers
 
         if ($this->names != '') {
             $teams = $this->names;
-
-            //Make sure there's at least 2 teams
-
-            if (count($teams) == 1) {
-                $teams[] = 'Team 2';
-            }
-
             $this->noTeams = count($teams);
-
         }
 
 
         //Calculate the size of the first full round - for example if you have 5 teams, then the first full round will consist of 4 teams
         $minimumFirstRoundSize = pow(2, ceil(log($this->noTeams) / log(2)));
         $this->noRounds = log($minimumFirstRoundSize, 2);
-        $noByesToAdd = $minimumFirstRoundSize - $this->noTeams;
 
-        //Add the byes to the teams array
-        for ($i = 0; $i < $noByesToAdd; $i++) {
-            $teams[] = null;
-        }
 
         //Order the teams in a seeded order - this is required regardless of whether it is a seeded tournament or not, as it prevents BYEs playing eachother
-        for ($i = 0; $i < log($this->noTeams / 2, 2); $i++) {
-
-            $out = array();
-
-            foreach ($teams as $player) {
-                $splice = pow(2, $i);
-                $out = array_merge($out, array_splice($teams, 0, $splice));
-                $out = array_merge($out, array_splice($teams, -$splice));
-            }
-
-            $teams = $out;
-
-        }
-
-        //Randomise the draw if required - keep the position of the byes, but swap the players
-
-//        if($this->type != 1){
-//
-//            $randomTeams = array();
-//            $shuffledTeams = array_filter($teams);
-//            shuffle($shuffledTeams);
-//
-//            foreach($teams as $key => $team){
-//                $randomTeams[$key] = is_null($team) ? null : array_pop($shuffledTeams);
-//            }
-//
-//            $teams = $randomTeams;
-//
-//        }
+        $teams = $this->orderTeamsInSeededOrder($teams);
 
         $roundNumber = 1;
 
         //Group 2 teams into a match
         $matches = array_chunk($teams, 2);
 
-        //Only check for BYEs if there are more than 2 teams
-        if ($this->noTeams > 2) {
-
-            foreach ($matches as $key => &$match) {
-
-                $matchNumber = $key + 1;
-
-                //If both teams are present, then that means they haven't had a BYE to the next round, so they must play in the first round
-                if ($match[0] && $match[1]) {
-
-                    //Add the match to the first round
-                    $this->brackets[$roundNumber][$matchNumber] = $match;
-
-                    //Set the match to null as the result of the above match hasn't yet been determined
-                    $match = null;
-
-                } else {
-
-                    //If only the first or second player exists, then replace the multidimensional array with the existing player
-                    $match = $match[0] ? $match[0] : $match[1];
-
-                }
-
-            }
-
-            //Now all of the blank spaces except the ones awaiting first round results have gone, group the single dimension array into a multiple dimensional array, so opponents share the same parent array
-            $matches = array_chunk($matches, 2);
-
-        }
 
         //If there's already a match in the match array, then that means the next round is round 2, so increase the round number
         if (count($this->brackets)) $roundNumber++;
-
+        $countMatches = count($matches);
         //Create the first full round of teams, some may be blank if waiting on the results of a previous round
-        for ($i = 0; $i < count($matches); $i++) {
+        for ($i = 0; $i < $countMatches; $i++) {
             $this->brackets[$roundNumber][$i + 1] = $matches[$i];
-        }
-
-        //Create the result of the empty rows for this tournament
-
-        for ($roundNumber += 1; $roundNumber <= $this->noRounds; $roundNumber++) {
-            for ($matchNumber = 1; $matchNumber <= ($minimumFirstRoundSize / pow(2, $roundNumber)); $matchNumber++) {
-                $this->brackets[$roundNumber][$matchNumber] = array(null, null);
-            }
         }
 
         $this->assignPositions();
@@ -225,6 +147,9 @@ class DirectEliminationTreeGen implements TreeGenerable
 
     }
 
+    /**
+     * Print Round Titles
+     */
     private function printRoundTitles()
     {
 
@@ -272,6 +197,10 @@ class DirectEliminationTreeGen implements TreeGenerable
 
     }
 
+    /**
+     * @param $selected
+     * @return string
+     */
     private function getPlayerList($selected)
     {
 
@@ -291,5 +220,29 @@ class DirectEliminationTreeGen implements TreeGenerable
 
         return $html;
 
+    }
+
+    /**
+     * @param $teams
+     * @return array
+     */
+    private function orderTeamsInSeededOrder($teams): array
+    {
+        $logNoTeam = log($this->noTeams / 2, 2);
+
+        for ($i = 0; $i < $logNoTeam; $i++) {
+
+            $out = [];
+
+            foreach ($teams as $player) {
+                $splice = pow(2, $i);
+                $out = array_merge($out, array_splice($teams, 0, $splice));
+                $out = array_merge($out, array_splice($teams, -$splice));
+            }
+
+            $teams = $out;
+
+        }
+        return $teams;
     }
 }
