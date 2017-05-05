d66f6c0d226282b7fb567d52984759d9ce10dc3e
Julien Cappiello <xoco70@hotmail.com>
2017-05-05 12:41:41 -0500

    refactor numPlayer partial

--- resources/views/partials/settings.blade.php
+++ resources/views/partials/settings.blade.php
@@ -41,23 +41,9 @@ if (session()->has('numFighters')) {
             <div class="input-group">
                 <label for="fighterQty">Fighter Qty</label>
                 <select class="form-control" id="numFighters" name="numFighters">
-                    <option value="1" @if ($numFighters== 1) selected @endif >1</option>
-                    <option value="2" @if ($numFighters== 2) selected @endif >2</option>
-                    <option value="3" @if ($numFighters== 3) selected @endif >3</option>
-                    <option value="4" @if ($numFighters== 4) selected @endif >4</option>
-                    <option value="5" @if ($numFighters== 5) selected @endif >5</option>
-                    <option value="6" @if ($numFighters== 6) selected @endif >6</option>
-                    <option value="7" @if ($numFighters== 7) selected @endif >7</option>
-                    <option value="8" @if ($numFighters== 8) selected @endif >8</option>
-                    <option value="8" @if ($numFighters== 9) selected @endif >9</option>
-                    <option value="8" @if ($numFighters== 10) selected @endif >10</option>
-                    <option value="8" @if ($numFighters== 11) selected @endif >11</option>
-                    <option value="8" @if ($numFighters== 12) selected @endif >12</option>
-                    <option value="8" @if ($numFighters== 13) selected @endif >13</option>
-                    <option value="8" @if ($numFighters== 14) selected @endif >14</option>
-                    <option value="8" @if ($numFighters== 15) selected @endif >15</option>
-                    <option value="8" @if ($numFighters== 16) selected @endif >16</option>
-                    <option value="8" @if ($numFighters== 17) selected @endif >17</option>
+                    @for($i=1;$i<18;$i++)
+                        <option value="{{$i}}" @if ($numFighters== $i) selected @endif >{{  $i }}</option>
+                    @endfor
                 </select>
             </div>
         </div>
