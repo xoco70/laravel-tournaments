428031d29bc42d15253aa6f2a14fcaabe760d630
Julien Cappiello <xoco70@hotmail.com>
2017-06-17 15:27:37 +0200

    new layout for preliminary

--- resources/views/partials/tree/preliminary.blade.php
+++ resources/views/partials/tree/preliminary.blade.php
@@ -1,52 +1,47 @@
 <?php
 $settings = $championship->getSettings();
 ?>
-<div class="row">
-    <div class="col-lg-6">
-        @if (Request::is('championships/'.$championship->id.'/pdf'))
-            <h1> {{$championship->buildName()}}</h1>
-        @endif
-        <br/>
-        <br/>
-        
-        @foreach($championship->fightersGroups()->where('round',1)->get()->groupBy('area') as $groupsByArea)
-            <table class="table-bordered" cellpadding="5" cellspacing="0">
-                <tr>
-                    <th class="p-10">Area</th>
-                    <th class="p-10"></th>
-                    <th class="p-10">Competitor 1</th>
-                    <th class="p-10"></th>
-                    <th class="p-10">Competitor 2</th>
-                    <th class="p-10"></th>
-                    <th class="p-10">Competitor 3</th>
-                    @if ($settings!= null && $settings->preliminaryGroupSize > 3)
-                        <th class="p-10"></th>
-                        <th class="p-10">Competitor 4</th>
-                    @endif
-                    @if ($settings!= null && $settings->preliminaryGroupSize > 4)
-                        <th class="p-10"></th>
-                        <th class="p-10">Competitor 5</th>
-                    @endif
-                </tr>
+@if (Request::is('championships/'.$championship->id.'/pdf'))
+    <h1> {{$championship->buildName()}}</h1>
+@endif
+<br/>
+<br/>
 
-                @foreach($groupsByArea as $group)
-                    <?php
-                    $names = $group->fightersWithBye()->map->fullName;
-                    $letter = 'a';
-                    ?>
-                    <tr>
-                        <td class="p-10">{{$group->area}}</td>
-                        @foreach ($names as $name)
-                            <td class="p-10">{{ $letter++ }}</td>
-                            <td class="p-10">{{ $name }}</td>
-                        @endforeach
-                    </tr>
+@foreach($championship->fightersGroups()->where('round',1)->get()->groupBy('area') as $groupsByArea)
+    <table class="table-bordered" cellpadding="5" cellspacing="0">
+        <tr>
+            <th class="p-10">Area</th>
+            <th class="p-10"></th>
+            <th class="p-10">Competitor 1</th>
+            <th class="p-10"></th>
+            <th class="p-10">Competitor 2</th>
+            <th class="p-10"></th>
+            <th class="p-10">Competitor 3</th>
+            @if ($settings!= null && $settings->preliminaryGroupSize > 3)
+                <th class="p-10"></th>
+                <th class="p-10">Competitor 4</th>
+            @endif
+            @if ($settings!= null && $settings->preliminaryGroupSize > 4)
+                <th class="p-10"></th>
+                <th class="p-10">Competitor 5</th>
+            @endif
+        </tr>
+
+        @foreach($groupsByArea as $group)
+            <?php
+            $names = $group->fightersWithBye()->map->fullName;
+            $letter = 'a';
+            ?>
+            <tr>
+                <td class="p-10">{{$group->area}}</td>
+                @foreach ($names as $name)
+                    <td class="p-10">{{ $letter++ }}</td>
+                    <td class="p-10">{{ $name }}</td>
                 @endforeach
-            </table><br/>
+            </tr>
         @endforeach
-    </div>
-    <div class="col-lg-6">
-        @include('kendo-tournaments::partials.tree.directElimination', ['hasPreliminary' => 1])
-    </div>
-</div>
+    </table><br/>
+@endforeach
+
+@include('kendo-tournaments::partials.tree.directElimination', ['hasPreliminary' => 1])
 
