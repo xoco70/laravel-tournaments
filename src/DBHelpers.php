ab55a2eb04eee7635557d0758515b97618197bc9
Julien Cappiello <xoco70@hotmail.com>
2017-08-25 18:22:57 +0200

    add DBhelpers class

---
+++ src/DBHelpers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Xoco70\KendoTournaments;
+
+use Illuminate\Support\Facades\DB;
+
+class DBHelpers
+{
+
+
+    static function setFKCheckOff()
+    {
+        switch (DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=0');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = OFF');
+                break;
+        }
+    }
+
+    static function setFKCheckOn()
+    {
+        switch (DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=1');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = ON');
+                break;
+        }
+    }
+}
