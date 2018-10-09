7599db962854ae1ec5b4be640fc58982f7694bba
Cappiello Julien <julien@cappiello.fr>
2018-10-09 21:02:52 +0200

    improve migration for user table

---
+++ database/migrations/2012_10_13_000005_alter_lt_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLtUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(Config::get('laravel-tournaments.user_table'), function (Blueprint $table) {
+            if (!Schema::hasColumn('name')) {
+                $table->string('name')->default('name');
+            }
+            if (!Schema::hasColumn('firstname')) {
+                $table->string('firstname')->default('firstname');
+            }
+            if (!Schema::hasColumn('lastname')) {
+                $table->string('lastname')->default('lastname');
+            }
+            if (!Schema::hasColumn('email')) {
+                $table->string('email')->unique();
+            }
+
+            if (!Schema::hasColumn('password')) {
+                $table->string('password', 60);
+            }
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
