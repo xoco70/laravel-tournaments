5ffe7e9731a98fa290a90801227ebddf3671eda8
Cappiello Julien <julien@cappiello.fr>
2018-10-10 15:09:17 +0200

    change name for migration alter user to run after create

---
+++ database/migrations/2014_10_12_000001_alter_lt_users_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLtUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable(config('laravel-tournaments.user_table'))) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('firstname')->default('firstname');
+                $table->string('lastname')->default('lastname');
+                $table->string('email')->unique();
+                $table->string('password', 60);
+                $table->timestamps();
+            });
+        } else {
+            Schema::table(config('laravel-tournaments.user.table'), function (Blueprint $table) {
+                if (!Schema::hasColumn(config('laravel-tournaments.user.table'), 'name')) {
+                    $table->string('name')->default('name');
+                }
+                if (!Schema::hasColumn(config('laravel-tournaments.user.table'), 'firstname')) {
+                    $table->string('firstname')->default('firstname');
+                }
+                if (!Schema::hasColumn(config('laravel-tournaments.user.table'), 'lastname')) {
+                    $table->string('lastname')->default('lastname');
+                }
+                if (!Schema::hasColumn(config('laravel-tournaments.user.table'), 'email')) {
+                    $table->string('email')->unique();
+                }
+
+                if (!Schema::hasColumn(config('laravel-tournaments.user.table'), 'password')) {
+                    $table->string('password', 60);
+                }
+            });
+        }
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
