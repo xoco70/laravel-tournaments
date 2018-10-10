82787954ac921d86ba2aa4760d01b9178d88dc1d
Cappiello Julien <julien@cappiello.fr>
2018-10-10 15:10:09 +0200

    change Auth\User to App\User ref

--- database/seeds/CompetitorSeeder.php
+++ database/seeds/CompetitorSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Foundation\Auth\User;
 use Xoco70\LaravelTournaments\Models\Championship;
 use Xoco70\LaravelTournaments\Models\Competitor;
 
@@ -18,11 +17,11 @@ class CompetitorSeeder extends Seeder
 
         $championship = Championship::where('tournament_id', 1)->first();
 
-        $users[] = factory(User::class)->create(['name' => 't1']);
-        $users[] = factory(User::class)->create(['name' => 't2']);
-        $users[] = factory(User::class)->create(['name' => 't3']);
-        $users[] = factory(User::class)->create(['name' => 't4']);
-        $users[] = factory(User::class)->create(['name' => 't5']);
+        $users[] = factory(App\User::class)->create(['name' => 't1']);
+        $users[] = factory(App\User::class)->create(['name' => 't2']);
+        $users[] = factory(App\User::class)->create(['name' => 't3']);
+        $users[] = factory(App\User::class)->create(['name' => 't4']);
+        $users[] = factory(App\User::class)->create(['name' => 't5']);
 
         foreach ($users as $user) {
             factory(Competitor::class)->create([
