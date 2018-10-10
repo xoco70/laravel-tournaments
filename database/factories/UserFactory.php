b1ac73e882b259607e8a7ba70bcd927ce027eb56
Julien <contact@kendozone.com>
2018-10-10 14:04:23 +0000

    Apply fixes from StyleCI

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,11 +5,11 @@ $factory->define(\App\User::class, function (Faker\Generator $faker) {
     $email = $faker->email;
 
     return [
-        'name'      => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name'              => $faker->name,
+        'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-        'lastname'  => $faker->lastName,
+        'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token'    => str_random(10),
+        'lastname'          => $faker->lastName,
     ];
 });
