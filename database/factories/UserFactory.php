404e3d1fdfd29287f469b90ed568bdd72483a397
Julien <5855577+xoco70@users.noreply.github.com>
2018-10-10 16:09:20 +0200

    Apply fixes from StyleCI (#29)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,14 +2,13 @@
 
 
 $factory->define(\App\User::class, function (Faker\Generator $faker) {
-
     return [
-        'name'              => $faker->name,
-        'email'             => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token'    => str_random(10),
+        'name'               => $faker->name,
+        'email'              => $faker->unique()->safeEmail,
+        'email_verified_at'  => now(),
+        'password'           => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token'     => str_random(10),
         'firstname'          => $faker->firstname,
-        'lastname'          => $faker->lastName,
+        'lastname'           => $faker->lastName,
     ];
 });
