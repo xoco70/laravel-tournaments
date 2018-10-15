4964113fa8c75fd00e7bcd560797a94551499b8d
Julien <5855577+xoco70@users.noreply.github.com>
2018-10-15 14:07:16 +0200

    Apply fixes from StyleCI (#32)

--- database/factories/TournamentFactory.php
+++ database/factories/TournamentFactory.php
@@ -5,19 +5,18 @@ use Xoco70\LaravelTournaments\Models\Tournament;
 use Xoco70\LaravelTournaments\Models\Venue;
 
 $factory->define(Tournament::class, function (Faker\Generator $faker) {
-
     $dateIni = $faker->dateTimeBetween('now', '+2 weeks')->format('Y-m-d');
     $venues = Venue::all()->pluck('id')->toArray();
 
     return [
-        'user_id' => factory(\Illuminate\Foundation\Auth\User::class)->create()->id,
-        'slug' => $faker->slug(2),
-        'name' => $faker->name,
-        'dateIni' => $dateIni,
-        'dateFin' => $dateIni,
+        'user_id'           => factory(\Illuminate\Foundation\Auth\User::class)->create()->id,
+        'slug'              => $faker->slug(2),
+        'name'              => $faker->name,
+        'dateIni'           => $dateIni,
+        'dateFin'           => $dateIni,
         'registerDateLimit' => $dateIni,
-        'sport' => 1,
-        'type' => $faker->boolean(),
-        'venue_id' => $faker->randomElement($venues),
+        'sport'             => 1,
+        'type'              => $faker->boolean(),
+        'venue_id'          => $faker->randomElement($venues),
     ];
 });
