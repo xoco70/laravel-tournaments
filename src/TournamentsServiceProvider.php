4b18f57a614366cc98dd9bbf9a317883845adfe9
Cappiello Julien <julien@cappiello.fr>
2018-10-21 14:13:28 +0200

    publish config file

--- src/TournamentsServiceProvider.php
+++ src/TournamentsServiceProvider.php
@@ -25,6 +25,7 @@ class TournamentsServiceProvider extends ServiceProvider
         $this->publishes([__DIR__.'/../database/seeds' => $this->app->databasePath().'/seeds'], 'laravel-tournaments');
         $this->publishes([__DIR__.'/../database/factories' => $this->app->databasePath().'/factories'], 'laravel-tournaments');
         $this->publishes([__DIR__.'/../resources/assets' => public_path('vendor/laravel-tournaments')], 'laravel-tournaments');
+        $this->publishes([__DIR__.'/../config/laravel-tournaments.php' => config_path('laravel-tournaments.php')], 'laravel-tournaments');
 
         $router->group(['prefix' => 'laravel-tournaments', 'middleware' => ['web']], function ($router) {
             $router->get('/', 'Xoco70\LaravelTournaments\TreeController@index')->name('tree.index');
